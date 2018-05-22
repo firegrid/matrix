ed192607088acaf13591ef5ba75aee6bc95067b4
firegrid <praveenkumar37@gmail.com>
2018-05-22 22:01:56 +0530

    set up initialized

--- arduino/cube.ino
+++ arduino/cube.ino
@@ -1,6 +1,19 @@
 
-void setup() {
+int row[] = {2, 3, 4, 5};
+int col[] = {6, 7, 8, 9};
+int layer[] = {10, 11, 12, 13}
 
+void setup() {
+  int i;
+  for (i = 0; i < 4; i++) {
+    pinMode(row[i], OUTPUT);
+  }
+  for (i = 0; i < 4; i++) {
+    pinMode(col[i], OUTPUT);
+  }
+  for (i = 0; i < count; i++) {
+    pinMode(layer[i], OUTPUT);
+  }
 }
 
 void loop() {
